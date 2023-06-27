6081a9f8d47b5f158bf5d361336722be355c24b3
Ravi Mahdu Sudhan Reddy <ravimadhusudhanreddy@gmail.com>
2023-06-27 18:00:43 +0530

    Add Project

---
+++ src/test/java/com/example/RemoteAPIs/RemoteApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RemoteAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
